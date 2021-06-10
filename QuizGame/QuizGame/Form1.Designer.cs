fb38015f0814b13a0499a368cf381a49ec887180
Filip <filipos.simonov@gmail.com>
2021-06-10 18:39:00 +0200

    Intial commit
    Adding the basics of the game.

---
+++ QuizGame/QuizGame/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace QuizGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(12, 455);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(268, 56);
+            this.btn1.TabIndex = 1;
+            this.btn1.Tag = "1";
+            this.btn1.Text = "button1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.checkAnswer);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(286, 455);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(266, 56);
+            this.btn2.TabIndex = 2;
+            this.btn2.Tag = "2";
+            this.btn2.Text = "button2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.checkAnswer);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(12, 538);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(268, 56);
+            this.btn3.TabIndex = 3;
+            this.btn3.Tag = "3";
+            this.btn3.Text = "button3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.checkAnswer);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(286, 538);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(266, 56);
+            this.button4.TabIndex = 4;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.checkAnswer);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 379);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(533, 61);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Кој е играчот на сликата?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QuizGame.Properties.Resources.ronaldo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(540, 353);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 606);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Click += new System.EventHandler(this.checkAnswer);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
